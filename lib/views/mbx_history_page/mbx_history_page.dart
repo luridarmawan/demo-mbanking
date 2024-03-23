fe309e538310284fb10294c0adca5d382f468543
Gustaman <>
2024-03-23 22:01:51 +0700

    History top corner radius

--- lib/views/mbx_history_page/mbx_history_page.dart
+++ lib/views/mbx_history_page/mbx_history_page.dart
@@ -10,51 +10,59 @@ class MbxHistoryPage extends StatelessWidget {
     return GetBuilder<MbxHistoryController>(
         init: MbxHistoryController(),
         builder: (controller) => ScreenX(
-              lightStatusBar: true,
-              loading: controller.historyListVM.list.isEmpty &&
-                  controller.historyListVM.loading,
-              headerView: NavigationBarX(
-                title: 'Riwayat',
-              ),
-              bottomPadding: false,
-              bodyView: NotificationListener<ScrollNotification>(
-                  onNotification: (ScrollNotification scrollInfo) {
-                    if (controller.historyListVM.loading == false &&
-                        scrollInfo.metrics.pixels ==
-                            scrollInfo.metrics.maxScrollExtent) {
-                      controller.nextPage();
-                    }
-                    return true;
-                  },
-                  child: Material(
-                    child: Scrollbar(
-                        child: ListView.separated(
-                            padding: EdgeInsets.only(
-                                bottom:
-                                    MediaQuery.of(Get.context!).padding.bottom +
+            lightStatusBar: true,
+            loading: controller.historyListVM.list.isEmpty &&
+                controller.historyListVM.loading,
+            headerView: NavigationBarX(
+              title: 'Riwayat',
+            ),
+            bottomPadding: false,
+            bodyView: ContainerX(
+                backgroundColor: ColorX.theme,
+                child: ContainerX(
+                  padding: EdgeInsets.only(top: 16.0),
+                  backgroundColor: ColorX.white,
+                  topLeftRadius: 16.0,
+                  topRightRadius: 16.0,
+                  child: NotificationListener<ScrollNotification>(
+                      onNotification: (ScrollNotification scrollInfo) {
+                        if (controller.historyListVM.loading == false &&
+                            scrollInfo.metrics.pixels ==
+                                scrollInfo.metrics.maxScrollExtent) {
+                          controller.nextPage();
+                        }
+                        return true;
+                      },
+                      child: Material(
+                        child: Scrollbar(
+                            child: ListView.separated(
+                                padding: EdgeInsets.only(
+                                    bottom: MediaQuery.of(Get.context!)
+                                            .padding
+                                            .bottom +
                                         100.0),
-                            physics: ClampingScrollPhysics(),
-                            separatorBuilder: (context, index) {
-                              return Padding(
-                                  padding: EdgeInsets.only(left: 60.0),
-                                  child: ContainerX(
-                                    height: 1.0,
-                                    width: double.infinity,
-                                    backgroundColor: ColorX.lightGray,
-                                  ));
-                            },
-                            itemCount: controller.historyListVM.list.length,
-                            itemBuilder: (context, index) {
-                              final history =
-                                  controller.historyListVM.list[index];
-                              return Material(
-                                  color: ColorX.transparent,
-                                  child: InkWell(
-                                      highlightColor: ColorX.highlight,
-                                      onTap: () {},
-                                      child: MbxHistoryCell(history)));
-                            })),
-                  )),
-            ));
+                                physics: ClampingScrollPhysics(),
+                                separatorBuilder: (context, index) {
+                                  return Padding(
+                                      padding: EdgeInsets.only(left: 60.0),
+                                      child: ContainerX(
+                                        height: 1.0,
+                                        width: double.infinity,
+                                        backgroundColor: ColorX.lightGray,
+                                      ));
+                                },
+                                itemCount: controller.historyListVM.list.length,
+                                itemBuilder: (context, index) {
+                                  final history =
+                                      controller.historyListVM.list[index];
+                                  return Material(
+                                      color: ColorX.transparent,
+                                      child: InkWell(
+                                          highlightColor: ColorX.highlight,
+                                          onTap: () {},
+                                          child: MbxHistoryCell(history)));
+                                })),
+                      )),
+                ))));
   }
 }
