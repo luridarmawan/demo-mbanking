175363f8f25d36670562fc181b825cf9f7102598
Gustaman <>
2024-03-28 13:58:52 +0700

    Theme fixed

--- lib/viewmodels/mbx_theme_vm.dart
+++ lib/viewmodels/mbx_theme_vm.dart
@@ -1,3 +1,4 @@
+import '../views/mbx_bottom_navbar_screen/mbx_bottom_navbar_controller.dart';
 import '../views/mbx_theme_sheet/mbx_theme_sheet.dart';
 import '../widgets/all_widgets.dart';
 import 'mbx_preferences_vm+users.dart';
@@ -8,6 +9,10 @@ class MbxThemeVM {
     final value = await sheet.show();
     await MbxUserPreferencesVM.setTheme(value);
     ColorX.theme = hexToColor(value);
+    try {
+      final controller = Get.find<MbxBottomNavBarController>();
+      controller.update();
+    } catch (e) {}
     return value;
   }
 }
