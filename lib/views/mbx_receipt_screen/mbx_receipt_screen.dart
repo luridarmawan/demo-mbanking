9b58fcdb86e47f5ec2166d48196647929306c60b
Gustaman <>
2024-04-02 14:48:19 +0700

    Receipt done

--- lib/views/mbx_receipt_screen/mbx_receipt_screen.dart
+++ lib/views/mbx_receipt_screen/mbx_receipt_screen.dart
@@ -25,59 +25,85 @@ class MbxReceiptScreen extends StatelessWidget {
               },
             ),
             bottomPadding: false,
-            bodyView: Column(children: [
-              ContainerX(
-                backgroundColor: ColorX.theme,
-                width: double.infinity,
-                height: 16.0,
-                child: ContainerX(
-                    backgroundColor: ColorX.white,
-                    topLeftRadius: 16.0,
-                    topRightRadius: 16.0),
-              ),
-              Expanded(
-                  child: Scrollbar(
-                child: SingleChildScrollView(
-                    child: Column(children: [
+            bodyView: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('lib/images/mbx_sticker.png'),
+                    repeat: ImageRepeat.repeat,
+                  ),
+                ),
+                child: Column(children: [
                   ContainerX(
-                    padding: EdgeInsets.all(4.0),
-                    borderWidth: 2.0,
-                    borderColor: ColorX.green,
-                    width: 70.0,
-                    height: 70.0,
-                    cornerRadius: 35.0,
-                    child: Center(
-                        child: ImageX(
-                      faIcon: FontAwesomeIcons.check,
-                      color: ColorX.white,
-                      width: 60.0,
-                      height: 60.0,
-                      cornerRadius: 30.0,
-                      backgroundColor: ColorX.green,
-                      padding: EdgeInsets.all(12.0),
-                    )),
+                    backgroundColor: ColorX.theme,
+                    width: double.infinity,
+                    height: 16.0,
+                    child: ContainerX(
+                        backgroundColor: ColorX.white,
+                        topLeftRadius: 16.0,
+                        topRightRadius: 16.0),
                   ),
-                  ContainerX(height: 16.0),
-                  TextX(
-                    controller.receiptVM.title,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w500,
-                  ),
-                  TextX(controller.receiptVM.amount,
-                      fontSize: 24.0, fontWeight: FontWeight.w600),
-                  ContainerX(height: 16.0),
-                  ListView.builder(
-                      shrinkWrap: true,
-                      padding: EdgeInsets.zero,
-                      physics: ClampingScrollPhysics(),
-                      itemCount: controller.receiptVM.details.length,
-                      itemBuilder: (context, index) {
-                        final detail = controller.receiptVM.details[index];
-                        return MbxReceiptCell(detail);
-                      }),
-                ])),
-              ))
-            ])));
+                  Expanded(
+                      child: Container(
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/pattern.png'),
+                              repeat: ImageRepeat.repeat,
+                            ),
+                          ),
+                          child: Scrollbar(
+                            child: SingleChildScrollView(
+                                child: Column(children: [
+                              ContainerX(
+                                padding: EdgeInsets.all(4.0),
+                                borderWidth: 2.0,
+                                borderColor: ColorX.green,
+                                width: 70.0,
+                                height: 70.0,
+                                cornerRadius: 35.0,
+                                child: Center(
+                                    child: ImageX(
+                                  faIcon: FontAwesomeIcons.check,
+                                  color: ColorX.white,
+                                  width: 60.0,
+                                  height: 60.0,
+                                  cornerRadius: 30.0,
+                                  backgroundColor: ColorX.green,
+                                  padding: EdgeInsets.all(12.0),
+                                )),
+                              ),
+                              ContainerX(height: 16.0),
+                              TextX(
+                                controller.receiptVM.title,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.w500,
+                              ),
+                              TextX(controller.receiptVM.amount,
+                                  fontSize: 24.0, fontWeight: FontWeight.w600),
+                              ContainerX(height: 16.0),
+                              ListView.builder(
+                                  shrinkWrap: true,
+                                  padding: EdgeInsets.zero,
+                                  physics: ClampingScrollPhysics(),
+                                  itemCount:
+                                      controller.receiptVM.details.length,
+                                  itemBuilder: (context, index) {
+                                    final detail =
+                                        controller.receiptVM.details[index];
+                                    return MbxReceiptCell(detail);
+                                  }),
+                              Padding(
+                                padding: EdgeInsets.all(24.0),
+                                child: ButtonX(
+                                  backgroundColor: ColorX.theme,
+                                  title: 'Kembali',
+                                  onClicked: () {
+                                    controller.btnBackClicked();
+                                  },
+                                ),
+                              ),
+                            ])),
+                          )))
+                ]))));
   }
 }
 
